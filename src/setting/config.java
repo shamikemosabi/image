af19eebcb7b079f601910041f800099a6373fc23
shamikemosabi <shamikemosabi@gmail.com>
2016-05-23 17:12:10 -0400

    update stat page to html, add link to stat page

--- src/setting/config.java
+++ src/setting/config.java
@@ -51,7 +51,7 @@ public class config
 	public final static String readFile =  System.getProperty("user.dir")+"\\read.txt";
 	public final static String configFile =  System.getProperty("user.dir")+"\\config.xml";
 	public final static String upgradeFile =  System.getProperty("user.dir")+"\\upgrade.xml";
-	public final static String statFile =  System.getProperty("user.dir")+"\\stat.txt";
+	public final static String statFile =  System.getProperty("user.dir")+"\\stat.html";
 	public final static String labFile =  System.getProperty("user.dir")+"\\lab.xml";
 	public final static String HashSER = System.getProperty("user.dir")+"\\hash.ser";
 	
